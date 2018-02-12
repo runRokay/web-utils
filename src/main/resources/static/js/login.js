6b77d898bf7a5c76d20025528f27160d658e372a
liaoyuecai <liaoyuecai.163.com>
2018-02-12 12:10:53 +0800

    auth edit

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,28 +1,39 @@
 $(function () {
-    $("#loginForm").validate({
-        rules: {
-            userName: "required",
-            password: {
-                required: true,
-                minlength: 6
+    $().ready(function () {
+        $("#loginForm").validate({
+            onsubmit: true,// 是否在提交是验证
+            onfocusout: false,// 是否在获取焦点时验证
+            onkeyup: false,// 是否在敲击键盘时验证
+
+            rules: {
+                userName: "required",
+                password: {
+                    required: true,
+                    minlength: 6
+                },
             },
-        },
-        messages: {
-            userName: "请输入用户名",
-            password: {
-                required: "请输入密码",
-                minlength: "密码长度不能小于 6 个字母"
+            messages: {
+                userName: "请输入用户名",
+                password: {
+                    required: "请输入密码",
+                    minlength: "密码长度不能小于 6 个字母"
+                },
             },
-        }
-    });
-    $("#saveDeviceForm").ajaxSubmit(function (data) {
-        data = JSON.parse(data);
-        if (data.success) {
-            layer.msg(data.message, {icon: 1}, function () {
-                window.location.href = "./index";
-            });
-        } else {
-            layer.msg(data.message, {icon: 2});
-        }
+            submitHandler: function (form) { //通过之后回调
+                $("#loginForm").ajaxSubmit(function (data) {
+                    data = JSON.parse(data);
+                    if (data.success) {
+                        layer.msg(data.message, {icon: 1}, function () {
+                            window.location.href = "./index";
+                        });
+                    } else {
+                        layer.msg(data.message, {icon: 2});
+                    }
+                });
+            },
+            invalidHandler: function (form, validator) {
+                return false;
+            }
+        });
     });
 })
